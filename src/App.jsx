d4b186e9ad4a0b5b78062e50106c4ebcb9045001
Rahil69 <raahil2005@hotmail.com>
2026-08-15 16:08:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function Nigga() {
+  return (
+    <div>
+      <h1>niggaSex</h1>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Nigga />
+      <Nigga />
+      <Nigga />
+      <Nigga />
+    </div>
+  );
+}
