0edd644449b099431bcc393c9172f4dbcffe580b
Rahil69 <raahil2005@hotmail.com>
2026-08-15 16:36:28 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://react.dev/images/docs/scientists/yXOvdOSs.jpg",
-  imageSize: 90,
-};
+let loggedIn = false;
+let content = <div>youre logged in </div>;
 
 const App = () => {
   return (
     <>
-      <div>
-        <h1>{user.name}</h1>
-        <img
-          className="avatar"
-          src={user.imageUrl}
-          alt={user.name}
-          style={{ width: user.imageSize, height: user.imageSize }}
-        />
-      </div>
+      <div>{loggedIn && content}</div>
     </>
   );
 };
