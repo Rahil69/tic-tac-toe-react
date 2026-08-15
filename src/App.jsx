6014a59b5cc1e8885c4bbcd4f75203a61f1ae07d
Rahil69 <raahil2005@hotmail.com>
2026-08-15 16:56:38 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { use, useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
   function myFunction() {
-    setCounter((prev) => (prev += 1));
+    setCounter((prev) => prev + 1);
   }
   return (
     <div>
