c0028919c2597dc9d8adac60833fc107899261a4
Rahil69 <raahil2005@hotmail.com>
2026-08-18 17:47:09 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setxIsNext] = useState(true);
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
     setSquares(nextSquares);
+    setxIsNext((prev) => !prev);
   }
   return (
     <>
