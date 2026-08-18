9014a0e434fe77d971ab126c6ca26426f8063d15
Rahil69 <raahil2005@hotmail.com>
2026-08-18 17:33:29 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import React, { useState } from "react";
 
-function Square({ value }) {
-  const handleCLick = (event) => {
-    console.log(event.target.textContent);
-  };
+function Square({ value, onSquareClick }) {
   return (
-    <button onClick={handleCLick} className="square">
+    <button onClick={onSquareClick} className="square">
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[1] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} onSquareClick={handleClick} />
+        <Square value={squares[2]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square value={squares[3]} onSquareClick={handleClick} />
+        <Square value={squares[4]} onSquareClick={handleClick} />
+        <Square value={squares[5]} onSquareClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square value={squares[6]} onSquareClick={handleClick} />
+        <Square value={squares[7]} onSquareClick={handleClick} />
+        <Square value={squares[8]} onSquareClick={handleClick} />
       </div>
     </>
   );
