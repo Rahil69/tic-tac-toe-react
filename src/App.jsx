f894c4201e4b4619ce687dd4eeb502450f8e126e
Rahil69 <raahil2005@hotmail.com>
2026-08-19 14:43:44 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ export default function Board() {
     const nextSquares = squares.slice();
 
     // if XisNext = true nextSquares[i] = X else itll be O
-    if (squares[i] !== null) {
+    // this if statement stops the function if the position is not null in the squares state.
+    if (squares[i]) {
       return;
     }
     xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
@@ -25,6 +26,13 @@ export default function Board() {
 
     // better to do prev => !prev but this works too
     setxIsNext(!xIsNext);
+  }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
   }
   return (
     <>
@@ -47,4 +55,23 @@ export default function Board() {
       </div>
     </>
   );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
